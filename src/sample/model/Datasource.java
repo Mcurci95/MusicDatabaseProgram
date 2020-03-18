004bf13cee1acd2132bd0c9d35a8488031836488
Mcurci95 <mcurci95@gmail.com>
2020-03-17 18:56:22 -0700

    Creating singleton

---
+++ src/sample/model/Datasource.java
@@ -0,0 +1,126 @@
+package sample.model;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Datasource {
+
+    public static final String DB_NAME = "music.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:/Users/marcocurci/IdeaProjects/JavaTutorials/src/MusicDatabase/" + DB_NAME;
+
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUM_ID = "_id";
+    public static final String COLUMN_ALBUM_NAME = "name";
+    public static final String COLUMN_ALBUM_ARTIST = "artist";
+    public static final int INDEX_ALBUM_ID = 1;
+    public static final int INDEX_ALBUM_NAME = 2;
+    public static final int INDEX_ALBUM_ARTIST = 3;
+
+    public static final String TABLE_ARTISTS = "artists";
+    public static final String COLUMN_ARTIST_ID = "_id";
+    public static final String COLUMN_ARTIST_NAME = "name";
+    public static final int INDEX_ARTIST_ID = 1;
+    public static final int INDEX_ARTIST_NAME = 2;
+
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONGS = "_id";
+    public static final String COLUMN_SONG_TRACK = "track";
+    public static final String COLUMN_SONG_TITLE = "title";
+    public static final String COLUMN_SONG_ALBUM = "album";
+    public static final int INDEX_SONG_ID = 1;
+    public static final int INDEX_SONG_TRACK = 2;
+    public static final int INDEX_SONG_TITLE = 3;
+    public static final int INDEX_SONG_ALBUMN = 4;
+
+    public static final int ORDER_BY_NONE = 0;
+    public static final int ORDER_BY_ASC = 1;
+    public static final int ORDER_BY_DESC = 2;
+
+    private Connection conn;
+
+    private static Datasource instance = new Datasource();
+
+    // Turning this into a Singleton
+    private Datasource() {
+
+    }
+
+    public static Datasource getInstance() {
+        return instance;
+    }
+
+    public boolean open() {
+        try {
+            conn = DriverManager.getConnection(CONNECTION_STRING);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Could not connect to DB");
+            e.getMessage();
+            return false;
+        }
+    }
+
+    public void close() {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Could not close connection");
+            e.getMessage();
+        }
+    }
+
+    public List<Artist> queryArtists(int sortOrder) {
+        StringBuilder sb = new StringBuilder("SELECT * FROM ");
+        sb.append(TABLE_ARTISTS);
+        if (sortOrder != ORDER_BY_NONE) {
+            sb.append(" ORDER BY ");
+            sb.append(COLUMN_ARTIST_NAME);
+            sb.append( " COLLATE NOCASE ");
+            if (sortOrder == ORDER_BY_DESC) {
+                sb.append("DESC");
+            } else {
+                sb.append("ASC");
+            }
+        }
+        try (Statement statement = conn.createStatement();
+            ResultSet results = statement.executeQuery(sb.toString()))  {
+
+            List<Artist> artists = new ArrayList<>();
+            while (results.next()) {
+                Artist artist = new Artist();
+                artist.setId(results.getInt(INDEX_ARTIST_ID));
+                artist.setName(results.getString(INDEX_ARTIST_NAME));
+                artists.add(artist);
+            }
+
+            return artists;
+        } catch (SQLException e) {
+            e.getMessage();
+            return null;
+        }
+
+    }
+
+    public List<String> queryAlbumsForArtists(String artistName) {
+        String query = String.format("SELECT %s.%s FROM %s INNER JOIN %s ON %s.%s = %s.%s WHERE %s.%s = '%s'",
+                                        TABLE_ALBUMS, COLUMN_ALBUM_NAME, TABLE_ALBUMS, TABLE_ARTISTS, TABLE_ARTISTS, COLUMN_ARTIST_ID, TABLE_ALBUMS, COLUMN_ALBUM_ARTIST,
+                                        TABLE_ARTISTS, COLUMN_ALBUM_NAME, artistName);
+        System.out.println(query);
+
+        try (Statement statement = conn.createStatement()) {
+            ResultSet results = statement.executeQuery(query);
+            List<String> albums = new ArrayList<>();
+            while (results.next()) {
+                String album = results.getString(1);
+                albums.add(album);
+            }
+            return albums;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
