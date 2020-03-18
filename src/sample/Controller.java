33a6ee489aefc2d083c0298c485f262a4c12ea89
Mcurci95 <mcurci95@gmail.com>
2020-03-17 23:08:29 -0700

    Startup

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import sample.model.Artist;
+import sample.model.Datasource;
+
 public class Controller {
+
+}
+
+class GetAllArtistsTask extends Task {
+    @Override
+    public ObservableList<Artist> call() {
+        return FXCollections.observableArrayList(
+                Datasource.getInstance().queryArtists(Datasource.ORDER_BY_ASC)
+        );
+    }
 }
--- src/sample/model/Datasource.java
+++ src/sample/model/Datasource.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Datasource {
 
     public static final String DB_NAME = "music.db";
-    public static final String CONNECTION_STRING = "jdbc:sqlite:/Users/marcocurci/IdeaProjects/JavaTutorials/src/MusicDatabase/" + DB_NAME;
+    public static final String CONNECTION_STRING = "jdbc:sqlite:/Users/marcocurci/IdeaProjects/MusicDatabaseProgram/" + DB_NAME;
 
     public static final String TABLE_ALBUMS = "albums";
     public static final String COLUMN_ALBUM_ID = "_id";
